822d77d14ac7522c93aa3422fbbbaa5aca21c324
luis daniel cera barros <ceraluis4@gmail.com>
2025-07-10 11:15:20 -0500

    btn-login/logout

--- tienda-productos/src/js/auth.js
+++ tienda-productos/src/js/auth.js
@@ -1,14 +1,24 @@
 // funcion para verificar si el usuario esta autenticado
 export function isAutenticated(){
+    if(localStorage.getItem("auth_token") !== null){
+        document.getElementById("btnLogin").style.display="none"
+        document.getElementById("btnLogout").style.display="inline"
+    }
     return localStorage.getItem("auth_token") !== null;
+    
 }
 
 // funcion para logearnos
 export function login(token){
+    document.getElementById("btnLogin").style.display="none"
+    document.getElementById("btnLogout").style.display="inline"
     localStorage.setItem("auth_token", token)
 }
 
 // funcion para cerrar sesión
 export function logout(){
+    document.getElementById("btnLogin").style.display="inline"
+    document.getElementById("btnLogout").style.display="none"
     localStorage.removeItem("auth_token")
+    window.location.reload()
 }
--- tienda-productos/src/js/main.js
+++ tienda-productos/src/js/main.js
@@ -1,5 +1,6 @@
 // importamos funcion router
 import { router } from "../routes/router";
+import { logout } from "./auth";
 
 document.addEventListener("includes-loaded", () => {
   // ejecutamos router inicialmente
@@ -7,10 +8,16 @@ document.addEventListener("includes-loaded", () => {
 
   // interceptamos clicks internos de pagina
   document.addEventListener("click", (e) => {
+    let $btnLogout = document.getElementById("btnLogout")
     if (e.target.matches("[data-link]")) {
       e.preventDefault();
       history.pushState(null, null, e.target.href);
       router();
+    }
+    // escuchar el boton
+    if(e.target === $btnLogout){
+      console.log("click en logout");
+      logout()
     }
   });
   // manejo de navegacion con botones de atras/adelante
